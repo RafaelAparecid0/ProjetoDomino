4e092b65702c36f615fe9d122fcf8741b8218285
Fabricio de Biasi <fabriciogb2002@gmail.com>
2020-08-26 15:16:58 -0300

    esqueleto do código

--- ProjetoDomino.c
+++ ProjetoDomino.c
@@ -1,15 +1,89 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-struct {
-  int lado1;
-  int lado2;
-};
 
-int main ()
-{
-  //printf ("[%f|%f]") Os lados das peças, exemplo: [5|4]
-  //Fabrizio2.0
-  //rafa2.0
-  //Very strange
+void menuInicial(){
+    int i;
+    
+    printf("--SEJA BEM VINDO JOGADOR!--\n");
+    printf("\n");
+    printf("Para começar, selecione uma opcao:\n");
+    scanf("%i", &i);
+
+    printf("\n");
+    printf("1-Comecar o jogo\n");
+    printf("2-Salvar\n");
+    printf("3-Sair\n");
+    switch(i){
+        case 1:
+        printf("Insira seu nome por favor:\n");//esta 
+        //linha pode estar seriamente errada...
+        //melhor chamarmos outra função pro usuario colocar o nome
+        //ou se alguem puder consertar..
+        scanf("%i", &i);
+        break;
+        case 2:
+        printf("\n");
+        case 3:
+        printf("n");
+        
+    }
+
+    return 0;
+}
+
+void limparTela(){
+    //espaço reservado para o comando de limpar a tela
+    //(esqueci o comando)-acho que o rafael sabe kk
+}
+
+/*struct{
+    int lado1;
+    int lado2;
+};*/
+
+int main(){
+            //COLOCAR O PRINTF APÓS ESSA COLUNA AÍ, PARA QUE POSSAMOS IMPRIMIR EM FORMATO DE DOMINÓ!
+    int p1 = [0|0];
+    int p2 = [1|1];
+    int p3 = [2|2];
+    int p4 = [3|3];
+    int p5 = [4|4];
+    int p6 = [5|5];
+    int p7 = [6|6];
+    int p8 = [0|6];
+    int p9 = [1|6];
+    int p10 = [2|6];
+    int p11 = [3|6];
+    int p12 = [4|6];
+    int p13 = [5|6];
+    int p14 = [3|4];
+    int p15 = [5|1];
+    int p16 = [4|2];
+    int p17 = [5|3];
+    int p18 = [5|4];
+    int p19 = [2|5];
+    int p20 = [1|2];
+    int p21 = [4|1];
+    int p22 = [1|0];
+    int p23 = [2|0];
+    int p24 = [3|0];
+    int p25 = [4|0];
+    int p26 = [5|0];
+    int p27 = [3|2];
+    int p28 = [1|3];
+
+    //Espaço para a formatação em printf
+
+
+
+
+    return 0;
+
+}
+
+void main(){
+    //Espaço reservado para construir o random!
+
+    return 0;
 }
