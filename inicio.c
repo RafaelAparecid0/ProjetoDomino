5623825209cc3ab7a23ce2fa91bb4a88c7593487
Fabricio de Biasi <fabriciogb2002@gmail.com>
2020-08-27 19:11:59 -0300

    agora sim!

--- inicio.c
+++ inicio.c
@@ -36,88 +36,88 @@ peca07.valor01 = 6;
 peca07.valor02 = 6;
 
 struct pecaDomino peca08;
-peca07.valor01 = 6;
-peca07.valor02 = 0;
+peca08.valor01 = 6;
+peca08.valor02 = 0;
 
 struct pecaDomino peca09;
-peca08.valor01 = 6;
-peca08.valor02 = 1;
+peca09.valor01 = 6;
+peca09.valor02 = 1;
 
 struct pecaDomino peca10;
-peca09.valor01 = 6;
-peca09.valor02 = 2;
+peca10.valor01 = 6;
+peca10.valor02 = 2;
 
 struct pecaDomino peca11;
-peca10.valor01 = 6;
-peca10.valor02 = 3;
+peca11.valor01 = 6;
+peca11.valor02 = 3;
 
 struct pecaDomino peca12;
-peca10.valor01 = 6;
-peca10.valor02 = 4;
+peca12.valor01 = 6;
+peca12.valor02 = 4;
 
 struct pecaDomino peca13;
-peca10.valor01 = 6;
-peca10.valor02 = 5;
+peca13.valor01 = 6;
+peca13.valor02 = 5;
 
 struct pecaDomino peca14;
-peca10.valor01 = 3;
-peca10.valor02 = 4;
+peca14.valor01 = 3;
+peca14.valor02 = 4;
 
 struct pecaDomino peca15;
-peca10.valor01 = 5;
-peca10.valor02 = 1;
+peca15.valor01 = 5;
+peca15.valor02 = 1;
 
 struct pecaDomino peca16;
-peca10.valor01 = 4;
-peca10.valor02 = 2;
+peca16.valor01 = 4;
+peca16.valor02 = 2;
 
 struct pecaDomino peca17;
-peca10.valor01 = 5;
-peca10.valor02 = 3;
+peca17.valor01 = 5;
+peca17.valor02 = 3;
 
 struct pecaDomino peca18;
-peca10.valor01 = 5;
-peca10.valor02 = 4;
+peca18.valor01 = 5;
+peca18.valor02 = 4;
 
 struct pecaDomino peca19;
-peca10.valor01 = 2;
-peca10.valor02 = 5;
+peca19.valor01 = 2;
+peca19.valor02 = 5;
 
 struct pecaDomino peca20;
-peca10.valor01 = 1;
-peca10.valor02 = 2;
+peca20.valor01 = 1;
+peca20.valor02 = 2;
 
 struct pecaDomino peca21;
-peca10.valor01 = 4;
-peca10.valor02 = 1;
+peca21.valor01 = 4;
+peca21.valor02 = 1;
 
 struct pecaDomino peca22;
-peca10.valor01 = 1;
-peca10.valor02 = 0;
+peca22.valor01 = 1;
+peca22.valor02 = 0;
 
 struct pecaDomino peca23;
-peca10.valor01 = 2;
-peca10.valor02 = 0;
+peca23.valor01 = 2;
+peca23.valor02 = 0;
 
 struct pecaDomino peca24;
-peca10.valor01 = 3;
-peca10.valor02 = 0;
+peca24.valor01 = 3;
+peca24.valor02 = 0;
 
 struct pecaDomino peca25;
-peca10.valor01 = 4;
-peca10.valor02 = 0;
+peca25.valor01 = 4;
+peca25.valor02 = 0;
 
 struct pecaDomino peca26;
-peca10.valor01 = 5;
-peca10.valor02 = 0;
+peca26.valor01 = 5;
+peca26.valor02 = 0;
 
 struct pecaDomino peca27;
-peca10.valor01 = 3;
-peca10.valor02 = 2;
+peca27.valor01 = 3;
+peca27.valor02 = 2;
 
 struct pecaDomino peca28;
-peca10.valor01 = 1;
-peca10.valor02 = 3;
+peca28.valor01 = 1;
+peca28.valor02 = 3;
 
 printf("[%d|%d]", peca01.valor01, peca01.valor02);
 printf("[%d|%d]", peca02.valor01, peca02.valor02);
