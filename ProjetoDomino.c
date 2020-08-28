f2b1e193967fcac5ec5ea92c99b9bb50d0282e54
SamSepiol <rafaelaparecido.oliveirasilva@gmail.com>
2020-08-28 16:22:52 -0300

    Leo1.0

--- ProjetoDomino.c
+++ ProjetoDomino.c
@@ -2,95 +2,20 @@
 #include<stdlib.h>
 
 //DECLARAÇÃO DO STRUCT COM AS PEÇAS
-struct pecas
+
+typedef struct Peca
 {
-    int vetor_pos_E[28];
-    int vetor_pos_D[28];
-} pecas_domino;
+    int esquerda;
+    int direita;
+}Peca[28];
 
+Peca pecas;
 
-void mostrar()
+/*Peca[] mostrar()
 {
-    int esquerda, direita, cont;
-    //printf("O sistema deverá permitir ao usuário mostrar todas as peças do dominó na tela (modo texto ou gráfico)");
-    1
-    // Sequencia de mapeamento dos indices das pedras do jogo de dominó
-    
-    // Sequencia de mapeamento dos indices das pedras do jogo de dominó.
-    
-    pecas_domino.vetor_pos_E[0] = 0; 
-    pecas_domino.vetor_pos_E[1] =0;
-    pecas_domino.vetor_pos_E[2] =0;
-    pecas_domino.vetor_pos_E[3] =0;
-    pecas_domino.vetor_pos_E[4] =0;
-    pecas_domino.vetor_pos_E[5] =0;
-    pecas_domino.vetor_pos_E[6] =0;
-    pecas_domino.vetor_pos_E[7] =1;
-    pecas_domino.vetor_pos_E[8] =1;
-    pecas_domino.vetor_pos_E[9] =1;
-    pecas_domino.vetor_pos_E[10]=1;
-    pecas_domino.vetor_pos_E[11]=1;
-    pecas_domino.vetor_pos_E[12]=1;
-    pecas_domino.vetor_pos_E[13]=2;
-    pecas_domino.vetor_pos_E[14]=2;
-    pecas_domino.vetor_pos_E[15]=2;
-    pecas_domino.vetor_pos_E[16]=2;
-    pecas_domino.vetor_pos_E[17]=2;
-    pecas_domino.vetor_pos_E[18]=3;
-    pecas_domino.vetor_pos_E[19]=3;
-    pecas_domino.vetor_pos_E[20]=3;
-    pecas_domino.vetor_pos_E[21]=3;
-    pecas_domino.vetor_pos_E[22]=4;
-    pecas_domino.vetor_pos_E[23]=4;
-    pecas_domino.vetor_pos_E[24]=4;
-    pecas_domino.vetor_pos_E[25]=5;
-    pecas_domino.vetor_pos_E[26]=5;
-    pecas_domino.vetor_pos_E[27]=6;
-    pecas_domino.vetor_pos_D[0]=0;
-    pecas_domino.vetor_pos_D[1]=1;
-    pecas_domino.vetor_pos_D[2]=2;
-    pecas_domino.vetor_pos_D[3]=3;
-    pecas_domino.vetor_pos_D[4]=4;
-    pecas_domino.vetor_pos_D[5]=5;
-    pecas_domino.vetor_pos_D[6]=6;
-    pecas_domino.vetor_pos_D[7]=1;
-    pecas_domino.vetor_pos_D[8]=2;
-    pecas_domino.vetor_pos_D[9]=3;
-    pecas_domino.vetor_pos_D[10]=4;
-    pecas_domino.vetor_pos_D[11]=5;
-    pecas_domino.vetor_pos_D[12]=6;
-    pecas_domino.vetor_pos_D[13]=2;
-    pecas_domino.vetor_pos_D[14]=3;
-    pecas_domino.vetor_pos_D[15]=4;
-    pecas_domino.vetor_pos_D[16]=5;
-    pecas_domino.vetor_pos_D[17]=6;
-    pecas_domino.vetor_pos_D[18]=3;
-    pecas_domino.vetor_pos_D[19]=4;
-    pecas_domino.vetor_pos_D[20]=5;
-    pecas_domino.vetor_pos_D[21]=6;
-    pecas_domino.vetor_pos_D[22]=4;
-    pecas_domino.vetor_pos_D[23]=5;
-    pecas_domino.vetor_pos_D[24]=6;
-    pecas_domino.vetor_pos_D[25]=5;
-    pecas_domino.vetor_pos_D[26]=6;
-    pecas_domino.vetor_pos_D[27]=6;
-    //esquerda = pecas_domino.vetor_pos_E[cont]; // Retorna o valor da pedra a esquerda de acordo com o indice do contador informado
-	//direita = pecas_domino.vetor_pos_D[cont]; // Retorna o valor da pedra a direita de acordo com o indice do contador informado
-
-    //TENTANDO IMPRIMIR AS PEÇAS DO DOMINÓ
-
-    //printf ("[%d]", pecas_domino.vetor_pos_D[27]); //DEU CERTO IMPRESSÃO
-
-    for (int i = 0; pecas_domino.vetor_pos_E[i] < 28; i++)
-    {
-        for (int j = 0; pecas_domino.vetor_pos_D[j] < 28; j++)
-        {
-            printf("[%d|%d]", pecas_domino.vetor_pos_D[i], pecas_domino.vetor_pos_D[j]);  
-        } 
-    }
-    system("pause");
+    peca nova ()
 }
-
+*/
 void organizar()
 
 {
