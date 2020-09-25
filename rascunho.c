dbd0dbd920f038f34b14c041edf6bd8fb452ccc2
Fabricio de Biasi <fabriciogb2002@gmail.com>
2020-09-25 09:54:16 -0300

    num de jogadores

--- rascunho.c
+++ rascunho.c
@@ -1,8 +1,21 @@
 #include<stdio.h>
-int main()
-{
-    printf("Vai assistir o Batman dublado, guei");    
-    printf ("SEILA");
-    printf("Ok Ok");
-    return 0;
+int  perguntaNumeroJodagores () {
+                              //AJUSTAR PARA APENAS 2 JOGADORES!!!!!!
+	int num_jogadores = 0 ;
+	bool bandeira;
+
+	faça {
+		printf ( " \ n \ n Computador - Quantas pessoas vão jogar? " );
+		printf ( " \ n Você - " );
+		scanf ( " % d% * c " , & num_jogadores);
+		bandeira = (num_jogadores < 2 ) || (num_jogadores> 4 );
+		if (bandeira)
+			printf ( " Computador - Só é possível jogar de 2 a 4 pessoas! " );
+
+	}
+	enquanto (bandeira);
+
+	printf ( " Computador - Beleza, vamos começar! \ n " );
+	return num_jogadores;
+
 }
